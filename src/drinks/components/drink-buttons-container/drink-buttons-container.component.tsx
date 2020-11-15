9beab313dddfaf7732564d8b225963b9b139b32f
glecko <alex.muelas@gmail.com>
2020-11-15 11:52:06 +0100

    feat: Add static images for drinks

--- src/drinks/components/drink-buttons-container/drink-buttons-container.component.tsx
+++ src/drinks/components/drink-buttons-container/drink-buttons-container.component.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ViewStyle } from 'react-native';
 import PredefinedDrinkButton from '../predefined-drink-button/predefined-drink-button.component';
-import { CUSTOM_DRINKS_BUTTON_IMAGE, PREDEFINED_DRINKS } from '../../data/predefined-drinks';
+import {
+  CUSTOM_DRINKS_BUTTON_PROPERTIES,
+  PREDEFINED_DRINKS
+} from '../../data/predefined-drinks';
 import styles from './drink-buttons-container.styles';
 import CustomActionButton from '../../../actions/components/action-register-button/custom-action/custom-action-button.component';
 import { ALCOHOL_UNIT_ACTION_TYPE } from '../../models/model';
@@ -11,14 +14,15 @@ const DrinkButtonsContainer = () => {
   const buttons = PREDEFINED_DRINKS.map((drink) => (
     <PredefinedDrinkButton {...drink} />
   ));
+  const customActionButtonStyle: ViewStyle = { ...buttonStyles.button, backgroundColor: CUSTOM_DRINKS_BUTTON_PROPERTIES.buttonColor };
   return (
     <View style={styles.container}>
       {buttons}
       <CustomActionButton
         type={ALCOHOL_UNIT_ACTION_TYPE}
-        image={CUSTOM_DRINKS_BUTTON_IMAGE}
-        text="Add custom drink"
-        style={buttonStyles.button}
+        image={CUSTOM_DRINKS_BUTTON_PROPERTIES.image}
+        text={CUSTOM_DRINKS_BUTTON_PROPERTIES.text}
+        style={customActionButtonStyle}
       />
     </View>
   );
--- src/drinks/data/predefined-drinks.ts
+++ src/drinks/data/predefined-drinks.ts
@@ -1,104 +1,108 @@
 import { AlcoholicDrinkModel } from '../models/model';
 
-export const CUSTOM_DRINKS_BUTTON_IMAGE = require('../../static/images/drink-icons/random-drinks.png');
+export const CUSTOM_DRINKS_BUTTON_PROPERTIES = {
+  image: require('@images/drink-icons/random-drinks.png'),
+  buttonColor: 'white',
+  text: 'Add custom drink'
+};
 
 export const PREDEFINED_DRINKS: AlcoholicDrinkModel[] = [
   {
     name: 'beer',
     volume: 200,
     content: 0.05,
-    image: require('../../static/images/drink-icons/beer-glass.png'),
+    image: require('@images/drink-icons/beer-glass.png'),
     buttonColor: '#FAE96F'
   },
   {
     name: 'bottle',
     volume: 330,
     content: 0.05,
-    image: require('../../static/images/drink-icons/beer-bottle.png'),
+    image: require('@images/drink-icons/beer-bottle.png'),
     buttonColor: '#FAE96F'
   },
   {
     name: 'pint',
     volume: 500,
     content: 0.05,
-    image: require('../../static/images/drink-icons/beer-jar.png'),
+    image: require('@images/drink-icons/beer-jar.png'),
     buttonColor: '#FAE96F'
   },
   {
     name: 'dark beer',
     volume: 200,
     content: 0.08,
-    image: require('../../static/images/drink-icons/beer-glass.png'),
+    image: require('@images/drink-icons/beer-glass.png'),
     buttonColor: '#F6C101'
   },
   {
     name: 'bottle (dark)',
     volume: 330,
     content: 0.08,
-    image: require('../../static/images/drink-icons/beer-bottle.png'),
+    image: require('@images/drink-icons/beer-bottle.png'),
     buttonColor: '#F6C101'
   },
   {
     name: 'pint (dark)',
     volume: 500,
     content: 0.08,
-    image: require('../../static/images/drink-icons/beer-jar.png'),
+    image: require('@images/drink-icons/beer-jar.png'),
     buttonColor: '#F6C101'
   },
   {
     name: '1/2 wine glass',
     volume: 100,
     content: 0.12,
-    image: require('../../static/images/drink-icons/wine-glass.png'),
+    image: require('@images/drink-icons/wine-glass.png'),
     buttonColor: '#ffb9b9'
   },
   {
     name: 'wine glass',
     volume: 200,
     content: 0.12,
-    image: require('../../static/images/drink-icons/wine-glass.png'),
+    image: require('@images/drink-icons/wine-glass.png'),
     buttonColor: '#ffb9b9'
   },
   {
     name: 'wine bottle',
     volume: 700,
     content: 0.12,
-    image: require('../../static/images/drink-icons/wine-bottle.png'),
+    image: require('@images/drink-icons/wine-bottle.png'),
     buttonColor: '#ffb9b9'
-  },
-  {
-    name: 'long drink',
-    volume: 70,
-    content: 0.35,
-    image: require('../../static/images/drink-icons/long-drink.png'),
-    buttonColor: 'white'
   },
   {
     name: 'spirit drink',
     volume: 100,
     content: 0.35,
-    image: require('../../static/images/drink-icons/whisky-glass.png'),
-    buttonColor: 'white'
-  },
-  {
-    name: 'cocktail',
-    volume: 100,
-    content: 0.17,
-    image: require('../../static/images/drink-icons/cocktail.png'),
-    buttonColor: 'white'
+    image: require('@images/drink-icons/whisky-glass.png'),
+    buttonColor: '#ebebeb'
   },
   {
     name: 'shot',
     volume: 20,
     content: 0.35,
-    image: require('../../static/images/drink-icons/shot-glass.png'),
-    buttonColor: 'white'
+    image: require('@images/drink-icons/shot-glass.png'),
+    buttonColor: '#ebebeb'
   },
   {
     name: 'double shot',
     volume: 40,
     content: 0.35,
-    image: require('../../static/images/drink-icons/shot-glass.png'),
-    buttonColor: 'white'
+    image: require('@images/drink-icons/shot-glass.png'),
+    buttonColor: '#ebebeb'
+  },
+  {
+    name: 'long drink',
+    volume: 70,
+    content: 0.35,
+    image: require('@images/drink-icons/long-drink.png'),
+    buttonColor: '#d94866'
+  },
+  {
+    name: 'cocktail',
+    volume: 100,
+    content: 0.17,
+    image: require('@images/drink-icons/cocktail.png'),
+    buttonColor: '#91ffad'
   },
 ];
